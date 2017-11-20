8fd3b888a5f8cf1dac789931fdaf13acb4b7c36b
bmr1334 <bmr1334@rit.edu>
2017-11-20 01:17:32 -0500

    E08 Deliverable

--- E08 - EntityManager/MyEntityManager.cpp
+++ E08 - EntityManager/MyEntityManager.cpp
@@ -7,6 +7,9 @@ void MyEntityManager::Init(void)
 }
 void MyEntityManager::Release(void)
 {
+	for (int i = 0; i < m_uEntityCount; i++) {
+		SafeDelete(m_entityList[i]);
+	}
 }
 MyEntityManager* MyEntityManager::GetInstance()
 {
@@ -18,6 +21,10 @@ MyEntityManager* MyEntityManager::GetInstance()
 }
 void MyEntityManager::ReleaseInstance()
 {
+	if (m_pInstance != nullptr){
+		SafeDelete(m_pInstance);
+		m_pInstance = nullptr;
+	}
 }
 int Simplex::MyEntityManager::GetEntityIndex(String a_sUniqueID)
 {
@@ -26,35 +33,41 @@ int Simplex::MyEntityManager::GetEntityIndex(String a_sUniqueID)
 //Accessors
 Model* Simplex::MyEntityManager::GetModel(uint a_uIndex)
 {
-	return nullptr;
+	return m_entityList[a_uIndex]->GetModel();
 }
 Model* Simplex::MyEntityManager::GetModel(String a_sUniqueID)
 {
-	return nullptr;
+	return MyEntity::GetEntity(a_sUniqueID)->GetModel();
 }
 RigidBody* Simplex::MyEntityManager::GetRigidBody(uint a_uIndex)
 {
-	return nullptr;
+	return m_entityList[a_uIndex]->GetRigidBody();
 }
 RigidBody* Simplex::MyEntityManager::GetRigidBody(String a_sUniqueID)
 {
-	return nullptr;
+	return MyEntity::GetEntity(a_sUniqueID)->GetRigidBody();
 }
 matrix4 Simplex::MyEntityManager::GetModelMatrix(uint a_uIndex)
 {
-	return IDENTITY_M4;
+	return m_entityList[a_uIndex]->GetModelMatrix();
 }
 matrix4 Simplex::MyEntityManager::GetModelMatrix(String a_sUniqueID)
 {
-	return IDENTITY_M4;
+	//if there's an entity with this ID
+	if (MyEntity::GetEntity(a_sUniqueID)) {
+		return MyEntity::GetEntity(a_sUniqueID)->GetModelMatrix();
+	}
+	else {
+		return IDENTITY_M4;
+	}
 }
 void Simplex::MyEntityManager::SetModelMatrix(matrix4 a_m4ToWorld, String a_sUniqueID)
 {
-
+	MyEntity::GetEntity(a_sUniqueID)->SetModelMatrix(a_m4ToWorld);
 }
 void Simplex::MyEntityManager::SetModelMatrix(matrix4 a_m4ToWorld, uint a_uIndex)
 {
-
+	m_entityList[a_uIndex]->SetModelMatrix(a_m4ToWorld);
 }
 //The big 3
 MyEntityManager::MyEntityManager(){Init();}
@@ -76,7 +89,7 @@ void Simplex::MyEntityManager::RemoveEntity(uint a_uIndex)
 }
 void Simplex::MyEntityManager::RemoveEntity(String a_sUniqueID)
 {
-
+	RemoveEntity((uint)GetEntityIndex(a_sUniqueID));
 }
 String Simplex::MyEntityManager::GetUniqueID(uint a_uIndex)
 {
@@ -84,7 +97,7 @@ String Simplex::MyEntityManager::GetUniqueID(uint a_uIndex)
 }
 MyEntity* Simplex::MyEntityManager::GetEntity(uint a_uIndex)
 {
-	return nullptr;
+	return m_entityList[a_uIndex];
 }
 void Simplex::MyEntityManager::AddEntityToRenderList(uint a_uIndex, bool a_bRigidBody)
 {
@@ -92,5 +105,5 @@ void Simplex::MyEntityManager::AddEntityToRenderList(uint a_uIndex, bool a_bRigi
 }
 void Simplex::MyEntityManager::AddEntityToRenderList(String a_sUniqueID, bool a_bRigidBody)
 {
-
+	MyEntity::GetEntity(a_sUniqueID)->AddToRenderList(a_bRigidBody);
 }
